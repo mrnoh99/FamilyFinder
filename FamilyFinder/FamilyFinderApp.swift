3438f4abc24e588836dac32c181d48db5b26c3f0
jai sung NOH <jsnoh@ajou.ac.kr>
2025-06-01 15:21:11 +0900

    Initial Commit

---
+++ FamilyFinder/FamilyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyFinderApp.swift
+//  FamilyFinder
+//
+//  Created by Jai Sung NOH on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FamilyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
